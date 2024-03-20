d3dbc7e07462da16b178dc0a1df46bab11e2e833
LudmilaStemp <stemporzhetskaia.lv@phystech.edu>
2024-03-20 15:58:51 +0300

    STLPrintf

--- C.cpp
+++ C.cpp
@@ -7,95 +7,12 @@ int main ()
 {
     printf ("begin\n");
 
-    //STLPrintf ("1234567890987654321234567890Hello ME-%s-%sOW%c%c%c0%s", "Ha-ha-ha",
-    //           "12345678901234567890123456789012345678901234567890");
-
-    int i = 0;
-    //for (int i = 0; i < 45; i++)
-    {
-        printf    ("%d %s %x %d %%%c%b\n",
-                   -1, "love", 3802, 100, 33, 31);
-        STLPrintf ("%d %s %x %d %%%c%b\n",
-                   -1, "love", 3802, 100, 33, 31);
-    }
-
-    STLPrintf ("%d %d %d %d%s", -5, 999, 888, -137456, "Aloha");
+    printf    ("%d %s %x %d %%%c%b\n",
+               -1, "love", 3802, 100, 33, 31);
+    STLPrintf ("%d %s %x %d %%%c%b\n",
+               -1, "love", 3802, 100, 33, 31);
 
     printf ("\nend");
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-// __cdecl
-
-
-    /**
-extern "C" void my_printf(const char* fmt);
-
-void my_printf(const char* fmt)
-{
-    printf("Your format string: %s\n", fmt);
-}       */
-
-
-
-           /**
-void as();
-
-typedef void (*func_t)();
-
-int main()
-{
-    int number = 1...16;
-
-    if ( number < 10 )
-    {
-        return number + '0';
-    }
-    else
-    {
-        return number - 10 + 'A';
-    }
-
-    char hex_digits[] = {'0', '1', '2', '3', ..., 'D', 'E', 'F'};
-    return hex_digits[number];
-
-
-
-
-    func_t handlers[] = {
-        as,
-
-
-
-
-
-
-
-
-        as,
-        as,
-        sd,
-
-
-
-
-
-        asd,
-        asd,
-    };
-
-    handler[option]();
-
-    return 0;
-}            */
-
